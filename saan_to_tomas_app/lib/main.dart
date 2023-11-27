b84d7ee9fb831a27a572c1650e1c7478c5a07924
Kess Entrata <kesselentrata@gmail.com>
2023-11-27 16:46:14 +0800

    add static home screen

--- saan_to_tomas_app/lib/main.dart
+++ saan_to_tomas_app/lib/main.dart
@@ -1,69 +1,15 @@
 import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
 
-void main() => runApp(const SaanToTomas());
+void main() {
+  runApp(MyApp());
+}
 
-class SaanToTomas extends StatelessWidget {
-  const SaanToTomas({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Final Academic Project',
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(0xF5, 0xF5, 0xF5, 0xFF),
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          title: const Text('SaanToTomas'),
-        ),
-        body: const SafeArea(
-          child: MapPage(),
-        ),
-      ),
-    );
-  }
-}
-
-class MapPage extends StatefulWidget {
-  const MapPage({super.key});
-
-  @override
-  MapPageState createState() => MapPageState();
-}
-
-class MapPageState extends State<MapPage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              height: 10,
-              color: Colors.white,
-              child: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Expanded(
-                    child: Icon(
-                      Icons.arrow_back
-                    ),
-                  ),
-                  Expanded(
-                      flex: 15,
-                      child: Text("Location", textAlign: TextAlign.center,)
-                  )
-                ],
-              ),
-            ),
-          ),
-          const Expanded(
-              flex: 8,
-              child: Text("data")
-          ),
-        ],
-      ),
+      home: HomeScreen(),
     );
   }
 }
